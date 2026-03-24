85aa368b66dc7da536d9f39f1d7b59df505b591a
OM <omjaiswal2552@gmail.com>
2026-03-24 20:14:05 +0530

    added backend

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World');
+})
+
+app.listen(3000, () => console.log('App is listening at port 3000'))
