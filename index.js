735f0262419285b39cc67a45bb464658576cca29
OM <omjaiswal2552@gmail.com>
2026-03-26 21:55:37 +0530

    added /hi route

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.get('/hello', (req, res) => {
     res.send('Hello World');
 })
 
+app.get('/hi', (req, res) => {
+    res.send('Hello Everyone')
+})
+
 app.listen(3000, () => console.log('App is listening at port 3000'))
